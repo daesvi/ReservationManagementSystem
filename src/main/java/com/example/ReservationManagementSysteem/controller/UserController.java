c3f116d8648210b1847f1e617747ed524875fe6f
daesvi <villegasdaniel4321@gmail.com>
2023-10-23 17:49:26 -0500

    Documentation with Swagger finished

--- src/main/java/com/example/ReservationManagementSysteem/controller/UserController.java
+++ src/main/java/com/example/ReservationManagementSysteem/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.ReservationManagementSysteem.controller;
 
-import com.example.ReservationManagementSysteem.model.AirlineEntity;
-import com.example.ReservationManagementSysteem.model.FlightEntity;
 import com.example.ReservationManagementSysteem.model.UserEntity;
 import com.example.ReservationManagementSysteem.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +21,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Create an user",
+            description = "Creates a new user and saves it to the database.")
+    @ApiResponse(responseCode = "201", description = "User created successfully",
+            content = @Content(schema = @Schema(implementation = UserEntity.class)))
+    @ApiResponse(responseCode = "500", description = "Internal Server Error")
     @PostMapping
-    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userEntity){
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userEntity) {
         UserEntity userCreated = userService.createUser(userEntity);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
