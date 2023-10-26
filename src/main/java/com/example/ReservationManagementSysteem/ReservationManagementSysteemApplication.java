0cbc7380ddf9f077e3a505dfe7e11750192594f7
daesvi <villegasdaniel4321@gmail.com>
2023-10-25 19:58:17 -0500

    CORS config is added

--- src/main/java/com/example/ReservationManagementSysteem/ReservationManagementSysteemApplication.java
+++ src/main/java/com/example/ReservationManagementSysteem/ReservationManagementSysteemApplication.java
@@ -4,7 +4,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ReservationManagementSysteemApplication {
@@ -18,6 +24,7 @@ public class ReservationManagementSysteemApplication {
         return new OpenAPI()
                 .info(new Info()
                         .title("Reservation Management System")
+                        .version("1.0.0")
                         .description("This is a reservation management system API developed in Java with the Spring framework, which allows\n" +
                                 "\n" +
                                 " - Create airlines\n" + "\n" +
@@ -25,6 +32,19 @@ public class ReservationManagementSysteemApplication {
                                 " - Manage flights\n" + "\n" +
                                 " - Search flights by search criteria\n" + "\n" +
                                 " - Reserve a flight")
+                        .termsOfService("http:/swagger.io/terms/")
                 );
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
